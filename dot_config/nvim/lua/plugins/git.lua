01489df68285c0752118d1291ab53a6e7caae506
Jonathan Balls <jonathanballs@protonmail.com>
2024-07-04 16:05:44 +0100

    Remove neogit

--- dot_config/nvim/lua/plugins/git.lua
+++ dot_config/nvim/lua/plugins/git.lua
@@ -5,14 +5,5 @@ return {
         config = function()
             require('gitsigns').setup()
         end
-    },
-    {
-        "NeogitOrg/neogit",
-        dependencies = {
-            "nvim-lua/plenary.nvim",  -- required
-            "sindrets/diffview.nvim", -- Diff integration
-            "ibhagwan/fzf-lua",
-        },
-        config = true
     }
 }
