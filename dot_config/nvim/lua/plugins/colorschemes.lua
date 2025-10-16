778d6c9f3de1c5dab1e604550c85189f5be4aa9d
Jonathan Balls <jonathanballs@protonmail.com>
2025-10-16 10:40:16 +0100

    enalbe kanagawa paper as default colorscheme

--- dot_config/nvim/lua/plugins/colorschemes.lua
+++ dot_config/nvim/lua/plugins/colorschemes.lua
@@ -4,12 +4,13 @@ return {
     { "sainnhe/everforest",               lazy = true },
     { "aktersnurra/no-clown-fiesta.nvim", lazy = true },
     { "rose-pine/neovim",                 name = "rose-pine", lazy = true },
+    { 'eddyekofo94/gruvbox-flat.nvim' },
     {
-        'eddyekofo94/gruvbox-flat.nvim',
+        "thesimonho/kanagawa-paper.nvim",
         priority = 1000,
         enabled = true,
         config = function()
-            vim.cmd([[colorscheme gruvbox-flat]])
+            vim.cmd([[colorscheme kanagawa-paper]])
         end,
     }
 }
